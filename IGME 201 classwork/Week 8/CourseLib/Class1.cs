dfc5ef95ad48fdde40c92f8875ead2384b18dae2
Kash <kasha@DESKTOP-TKSG659>
2023-10-12 14:25:35 -0400

    Help

--- IGME 201 classwork/Week 8/CourseLib/Class1.cs
+++ IGME 201 classwork/Week 8/CourseLib/Class1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 namespace CourseLib
 {
     public class Courses //this is going to have a sorted list for the string which is the course
-                         //code and the Course object (instance)****
+                         //code and the Course object (instance) which is the value (1)****
     {
         SortedList<string, Course> sortedList = new SortedList<string, Course>();
 
@@ -17,7 +18,8 @@ namespace CourseLib
         {
             if(courseCode != null)
             {
-                sortedList.Remove(courseCode);//****
+                sortedList.Remove(courseCode); // does this get the instance and remove it if it exists? (we are not accessing the key in this case
+                                               // or does the courseCode refer to the key in this case since its a string??)(2)****
             }
         }
 
@@ -28,14 +30,14 @@ namespace CourseLib
                 Course returnVal; 
                 try
                 {
-                    returnVal = (Course)sortedList[courseCode]; 
+                    returnVal = (Course)sortedList[courseCode]; //why do we have to explicitly cast here(3)***** 
                 }
                 catch
                 {
                     returnVal = null;
                 }
 
-                return (returnVal);
+                return (returnVal); //if it does not work is returns null otherwise it returns the course instance(4)??*****
             }
 
             set
@@ -50,7 +52,71 @@ namespace CourseLib
                 }
             }
         }
+
+        public Courses(); //is this how we implement the deault constructor why am I getting an error(5)****
     }
 
-    //do the rest after courses parent class****
+    public class Schedule 
+    {
+        public DateTime startTime; //what does DateTime refer to in this case(6)****
+        public DateTime endTime;
+        List<DayOfWeek> daysOfWeek = new List<DayOfWeek>(); //should we make this public
+        //so it works with the constructor in Course??)(7)****
+        //how does this list work(8)****
+    }
+
+    public class Course 
+    {
+        public courseCode string; //are these correct why are these error(9)****
+        public description string;
+        public teacherEmail string;
+        public schedule Schedule;
+
+        public Course(); //how do we know if its public or not by default because there is not specification in shumul(10)****
+                         //is this in addition to the default constructor****
+                         //we have to copy and paste(11)**
+        public Course(string courseCode, string description); //is this how we create the constructor for the Course Class(12)****
+
+        public Courses()
+        {
+            Course thisCourse;
+            Schedule thisSchedule;
+
+            Random rand = new Random();
+
+            // generate courses IGME-200 through IGME-299
+            for (int i = 200; i < 300; ++i)
+            {
+                // use constructor to create new course object with code and description
+                thisCourse = new Course(($"IGME-{i}"), ($"Description for IGME-{i}"));
+
+                // create a new Schedule object
+                thisSchedule = new Schedule();
+                for (int dow = 0; dow < 7; ++dow)
+                {
+                    // 50% chance of the class being on this day of week
+                    if (rand.Next(0, 2) == 1)
+                    {
+                        // add to the daysOfWeek list
+                        thisSchedule.daysOfWeek.Add((DayOfWeek)dow);
+
+                        // select random hour of day
+                        int nHour = rand.Next(0, 24);
+
+                        // set start and end times of minute duration
+                        // select fixed date to allow time calculations
+                        thisSchedule.startTime = new DateTime(1, 1, 1, nHour, 0, 0);
+                        thisSchedule.endTime = new DateTime(1, 1, 1, nHour, 50, 0);
+                    }
+                }
+
+                // set the schedule for this course
+                thisCourse.schedule = thisSchedule;
+
+                // add this course to the SortedList
+                this[thisCourse.courseCode] = thisCourse;
+            }
+        }
+
+    }
 }
---
+++ IGME 201 classwork/Week 8/MyDerivedClass/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDerivedClass
+{
+
+    public class MyClass
+    {
+        private string myString = "random string"; //we cant define private wtihin
+        //the method but its only accessible within the class*****
+        public virtual string GetString()
+        {
+            return myString;
+        }
+
+        public string MyString
+        {
+            set
+            {
+                myString = value; //what is value used for again*****
+            }
+        }
+
+        //derive a class means a child class*****
+        public class MyDerivedClass: MyClass
+        {
+            public override string GetString() //when we override we have to same exact signature as virtual
+                //or abstract but we have to say over ride instead of abstract or virtual******
+            {
+                return base.GetString();
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDerivedClass derived = new MyDerivedClass();//*****
+            string newStr = derived.GetString();
+            Console.WriteLine(newStr);//is this how we do it****
+        }
+    }
+}
